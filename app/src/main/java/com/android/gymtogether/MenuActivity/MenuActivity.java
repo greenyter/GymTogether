8483b12397601a4ccec6c5d72f12767e4fc13c1d
green <rawcio777@o2.pl>
2022-04-03 01:12:14 +0200

    Added logout

--- app/src/main/java/com/android/gymtogether/MenuActivity/MenuActivity.java
+++ app/src/main/java/com/android/gymtogether/MenuActivity/MenuActivity.java
@@ -1,21 +1,31 @@
 package com.android.gymtogether.MenuActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.view.View;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.android.gymtogether.LoginGoogleActivity.LoginGoogleActivity;
 import com.android.gymtogether.LoginGoogleActivity.model.User;
 import com.android.gymtogether.MenuActivity.presenter.MenuPresenter;
 import com.android.gymtogether.MenuActivity.presenter.MenuPresenterImpl;
 import com.android.gymtogether.MenuActivity.view.MenuView;
 import com.android.gymtogether.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MenuActivity extends AppCompatActivity implements MenuView {
 
     private CircleImageView profileImage;
     private MenuPresenter menuPresenter;
-
+    private GoogleSignInClient mGoogleSignInClient;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +37,27 @@ public class MenuActivity extends AppCompatActivity implements MenuView {
 
         menuPresenter.convertUriToImage(user.getPhotoUrl());
 
+    }
+
+    public void button(View v){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleSignInClient.signOut()
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                             a();
+                        }
+
+                    });
+
+    }
+
+    public void a(){
+        intent = new Intent(this,LoginGoogleActivity.class);
+        startActivity(intent);
     }
 
     private void initControllers(){
